db91609e0cbcce57b0529cc8e42db50d20dfdab6
Iraqi1994 <131317527+Iraqi1994@users.noreply.github.com>
2025-10-13 15:52:38 +0200

    Testing placement of chicken with random method.

--- models/world.class.js
+++ models/world.class.js
@@ -1,13 +1,21 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
+  canvas;
   ctx;
   constructor(canvas) {
+    this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.draw();
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+    this.enemies.forEach((chicken) => {
+      this.ctx.drawImage(chicken.img, chicken.x, chicken.y, chicken.width, chicken.height);
+    });
+
+    requestAnimationFrame(() => this.draw());
   }
 }
