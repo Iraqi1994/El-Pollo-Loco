b05bd3e6903adc52bf04fa4d27ed8971bbcfde6f
Iraqi1994 <131317527+Iraqi1994@users.noreply.github.com>
2025-10-15 22:07:48 +0200

    added animation for clouds character and chicken. moveLeft is now in movable-object.class.js. Chicken are moving left now.

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,8 +2,29 @@ class Chicken extends MovableObject {
   y = 350;
   width = 80;
   height = 80;
+  IMAGES_WALKING = [
+    "../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
+
   constructor() {
     super().loadImage("../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
     this.x = 200 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.25;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+      if (this.currentImage >= this.IMAGES_WALKING.length) {
+        this.currentImage = 0;
+      }
+      let path = this.IMAGES_WALKING[this.currentImage];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 200);
   }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,16 +4,30 @@ class MovableObject {
   height = 150;
   width = 100;
   img;
+  imageCache = {};
+  currentImage = 0;
+  speed = 0.15;
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
   }
 
+  loadImages(imageCache) {
+    imageCache.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
   moveRight() {
     console.log("move right");
   }
+
   moveLeft() {
-    console.log("move left");
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
   }
 }
