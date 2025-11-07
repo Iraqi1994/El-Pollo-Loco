e9139619bd36a7ed9395b0d24a46033179267ac4
Iraqi1994 <131317527+Iraqi1994@users.noreply.github.com>
2025-11-07 12:34:27 +0100

    Increased the amount of enemies.

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,17 @@
 const level1 = new Level(
-  [new Chick(), new Chicken(), new Chicken(), new Chicken(), new Endboss()],
+  [
+    new Chick(),
+    new Chick(),
+    new Chick(),
+    new Chick(),
+    new Chick(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Endboss(),
+  ],
   [new Cloud()],
   [
     new BackgroundObject("img/5_background/layers/air.png", -719),
