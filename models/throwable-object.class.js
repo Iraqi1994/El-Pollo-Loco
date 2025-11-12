de7eada5768701b7e9c3d4621bfc02aa7697b943
Iraqi1994 <131317527+Iraqi1994@users.noreply.github.com>
2025-11-12 14:41:47 +0100

    Added IMAGES_THROWING and IMAGES_SPLASH images for the throwing animaion.

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,8 +1,26 @@
 class ThrowableObject extends MovableObject {
   speedX = 15;
 
+  IMAGES_THROWING = [
+    "../img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "../img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "../img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "../img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  IMAGES_SPLASH = [
+    "../img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "../img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "../img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "../img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "../img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "../img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+  ];
+
   constructor(x, y) {
     super().loadImage("../img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+    this.loadImages(this.IMAGES_THROWING);
+    this.loadImages(this.IMAGES_SPLASH);
     this.x = x;
     this.y = y;
     this.height = 80;
