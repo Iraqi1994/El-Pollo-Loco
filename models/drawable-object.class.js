f4c67923bd15916a83fa9d1807c5667eb523da2d
Hans Müller <iraqihu1994@gmail.com>
2025-11-27 18:03:45 +0100

    Fixed a problem where the drawImage function would start drawing before all images where loaded.

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -25,7 +25,9 @@ class DrawableObject {
   }
 
   draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    if (this.img && this.img.complete && this.img.naturalHeight !== 0) {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
   }
 
   drawFrame(ctx) {
